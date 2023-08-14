46f6d3586658cebcc3de61cd30651dd6e7fdf4bc
alexandr-tchestnoff <sasha142251@gmail.com>
2023-08-14 14:13:35 +0300

    3

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '6654939050:AAHh2oEf6OHb0fYmlLUPa_M2FufivNBmieE'
+
+keys = {
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
